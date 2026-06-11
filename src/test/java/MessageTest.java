a6c68d758d8c9ab06bccc3cc9c6870a7143a09bd
kgopotso-mashile <st10524411@rcconnect.edu.za>
2026-06-11 20:35:45 +0200

    Added Part3 MessageTests
    Added multiple tests for message handling, including length checks, recipient validation, message hashing, and reporting.

--- src/test/java/MessageTest.java
+++ src/test/java/MessageTest.java
@@ -177,3 +177,141 @@ public void testMessageStoreOption() {
 
     assertEquals("Message stored successfully", result);
 }
+
+//-----------------------------------------------------MESSAGE TEST--------------------------------------------------------------------------------------
+      
+    @Test
+    public void testMessageLengthSuccess() { 
+    //Checks if the message is no more than 250 charecters in length
+    //The expected results is: message is ready to send 
+        assertEquals("Message is ready to send", message.checkMessageLength("HI!"));
+       }  
+    
+    @Test
+    public void testMessageLengthFail() { 
+    //Checks if the message is more than 250 charecters in length
+    String longMessage = "A". repeat(251);
+    String result = message.checkMessageLength(longMessage);
+        assertTrue(result.contains("251"), result);
+    }
+    
+//----------------------------------------------RECIPIENT NUMBER TEST-------------------------------------------------------------------
+    
+    
+    @Test
+    public void testValidRecipientNumber() { 
+        //this checks if the recipient number is valid
+        //Expected results are: Message Sucess
+        assertEquals("Cell phone number successfully added.", message.checkRecipientCell("+27838967986")); 
+     
+    }
+    
+    @Test
+    public void testInvalidRecipientNumber() { 
+        //Checks if the recipient number is incorrect
+        //Expected results is: a failure message
+         assertEquals("Cellphone number is incorrectly formatted", message.checkRecipientCell("0823344556"));
+    }
+    
+    
+//-----------------------------------------------MESSAGE HASH TEST------------------------------------------------------------------------------
+    
+    @Test
+    public void testCorrectMessageHash() { 
+        //Checks if the message hash is correct
+        //Expected results is: "00:0:HITONIGHT" for test data1
+         assertEquals("00:0:HITONIGHT", message.createMessageHash(0, "Hi tonight")); 
+    }
+
+    
+//-----------------------------------------------MESSAGE ID TEST-------------------------------------------------------------------
+        @Test
+    public void testMessageIDCreated() { 
+        //Checks if the message ID is created and is the correct length
+        String messageID = message.createMessageID();
+        assertNotNull(messageID);
+        assertEquals(10, messageID.length());
+    }
+    
+//--------------------------------------------SENT MESSAGES TEST-----------------------------------------------------------
+    
+        @Test
+        public void testSentMessages() { 
+        //Checks if the message is successfully sent when "Send" is chosen as an option
+        //Expected results is: Message is successfully sent
+         assertEquals("Message successfully sent.", message.sentMessage (1, "Hi tonight","+27838967986" ));
+        }
+
+        @Test
+        public void testDisregardMessage() { 
+        //Checks if the message is disregarded when "Disregard" is chosen as an option
+        //Expected results is: Press 0 to delete
+         assertEquals("Press 0 to delete the message.", message.sentMessage (2, "Hi tonight", "+27838697689"));
+        }
+        
+        @Test
+        public void testStoredMessage() { 
+        //Checks if the message is sucessfully stored when "Store" is chosen as an option
+        //Expected results is: Message sucessfully stored
+         assertEquals("Message sucessfully stored.", message.sentMessage (3, "Hi tonight", "+27838697689"));
+        }
+
+//====================================================================================================================================
+//                                                         PART3 TESTS
+//====================================================================================================================================
+
+
+    @Test 
+    public void testSentMessageArray_correctlyPopulated() { 
+        //This checks if the sentMessage array contains only messages flagged as sent 
+        //Expected results: "Did you get the cake?" and "It is dinner time"are present 
+            assertTrue(manager.getSentMessages().contains("Did you get the cake?") && manager.getSentMessages().contains("It is dinner time!")); 
+    }
+
+
+    //----------------------------------------------LONGEST MESSAGE TEST---------------------------------------------------------------------------
+        
+        @Test 
+    public void testDisplayLongestMessage_returnsCorrectMessages() { 
+        //This checks if dispaly longest message displays the longest message 
+        //Expected results: "Where are you? You are late! I have asked you to be on time." 
+            assertEquals("Where are you? You are so late! I have asked you to be on time", manager.displayLongestMessage());
+    } 
+
+        @Test 
+    public void testSearchByMessageID_returnCorrectMessage() { 
+        //This checks if searching by messsage 4s ID returns the correct message
+        //Expected results: "It is dinner time"
+            assertEquals("It is dinner time!", manager.searchByMessageID("0838884567"));
+    }
+    
+//-----------------------------------------------------SEARCH BY RECIPIENT TEST------------------------------------------------------------------------------
+    
+
+    @Test
+    public void testSearchByRecipient_returnsAllMatchingMessages() { 
+        //This checks if the recipient "+278388845667" returns all matching messages
+        String results = manager.searchByRecipient("+27838884567"); 
+                assertTrue(results.contains("Where are you? You are late! I have asked you to be on time") && results.contains("Ok, I amleaving without you."));
+    }
+    
+//--------------------------------------------------------DEPLETE BY HASH TEST-------------------------------------------------------------
+    
+
+    @Test
+    public void testDepleteByHash_removeCorrectMessages() { 
+        //Checks if deleting messages by 2 removes the correct messages
+        assertEquals("Messsage: Where are you? You are late! I have asked ypu to be on time successfully deleted.", manager.dleteByHash(message.getMessageHash()));
+    }
+
+    
+    @Test
+    public void testDisplayReport_containsRequiredFields() { 
+        //This checks if the report contains the hash, the recipient and a messsage sent for all messages
+        //Expected message is: the report string contains the hash, recipient and message text for messages 1 and 4. 
+        String report = manager.displayReport(); 
+            assertTrue(report.contains(message.getMessageHash()) && report.contains("+27838884567") && report.contains("Did you get the cake?") && report.contains(message.getMessageHash())
+&& report.contains("0838884567") && report.contains("It is dinner time!"));                     
+       
+}
+}
